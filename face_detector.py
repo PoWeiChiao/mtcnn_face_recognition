ff2e7d9fc7efcaf6c719875920395208a03d23c0
Po-Wei Chiao <po-wei.chiao@leica-microsystems.com>
2025-02-04 16:40:54 +0800

    update face_detector

--- face_detector.py
+++ face_detector.py
@@ -1,5 +1,6 @@
 import cv2
 import os
+from sympy import Point
 import torch
 from facenet_pytorch import InceptionResnetV1, MTCNN
 
@@ -41,9 +42,9 @@ class FaceDetector:
                 batch_boxes, batch_probs, batch_points, img, method=self.selection_method
             )
         faces = self.mtcnn.extract(img, batch_boxes, save_path)
-        return batch_boxes, faces
+        return batch_boxes, faces, batch_points
 
-    def detect(self, cam: int = 0, thres: float = 0.9) -> None:
+    def detect(self, cam: int = 0, thres: float = 0.9, face_recognition: bool = True, face_points: bool = False) -> None:
         """Capture video from a camera, detect faces in real-time, and label them."""
         vdo = cv2.VideoCapture(cam)
         while vdo.isOpened():
@@ -51,22 +52,26 @@ class FaceDetector:
             if not ret:
                 break
 
-            batch_boxes, cropped_images = self._detect_box(img0)
+            batch_boxes, cropped_images, batch_points = self._detect_box(img0)
             if batch_boxes is not None:
-                for box, cropped in zip(batch_boxes, cropped_images):
+                for box, cropped, points in zip(batch_boxes, cropped_images, batch_points):
                     x, y, x2, y2 = map(int, box)
-                    img_embedding = self._encode(cropped.unsqueeze(0))
-
                     min_key = 'Undetected'
-                    if self.all_people_faces:
-                        detect_dict = {k: (v - img_embedding).norm().item() for k, v in self.all_people_faces.items()}
-                        min_key, min_value = min(detect_dict.items(), key=lambda item: item[1])
-                        if min_value >= thres:
-                            min_key = 'Undetected'
+                    if face_recognition:
+                        img_embedding = self._encode(cropped.unsqueeze(0))
+                        if self.all_people_faces:
+                            detect_dict = {k: (v - img_embedding).norm().item() for k, v in self.all_people_faces.items()}
+                            min_key, min_value = min(detect_dict.items(), key=lambda item: item[1])
+                            if min_value >= thres:
+                                min_key = 'Undetected'
 
                     color = (0, 255, 0) if min_key != 'Undetected' else (0, 0, 255)
                     cv2.rectangle(img0, (x, y), (x2, y2), color, 2)
-                    cv2.putText(img0, min_key, (x + 5, y + 10), cv2.FONT_HERSHEY_DUPLEX, 0.5, (255, 255, 255), 1)
+                    if face_recognition:
+                        cv2.putText(img0, min_key, (x + 5, y + 10), cv2.FONT_HERSHEY_DUPLEX, 0.5, (255, 255, 255), 1)
+                    if face_points:
+                        for point in points:
+                            cv2.circle(img0, tuple(map(int, point)), 10, (255, 255, 255), -1)
 
             cv2.imshow("output", img0)
             if cv2.waitKey(1) & 0xFF == ord('q'):
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from face_detector import FaceDetector
 def main() -> None:
     """Initialize FaceDetector and start detection using the default camera."""
     fd = FaceDetector()
-    fd.detect(cam=0)
+    fd.detect(cam=0, thres=0.9, face_recognition=True, face_points=False)
 
 if __name__ == "__main__":
     main()
